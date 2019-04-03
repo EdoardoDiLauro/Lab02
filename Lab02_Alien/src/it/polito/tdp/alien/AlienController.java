d8bc38ffeea6cd1dc8e3c2883c4fefca35a03f58
Edoardo <Edoardo@DESKTOP-3KALHQP>
2019-04-04 01:49:42 +0200

    es1 + es2 + opt OK

--- Lab02_Alien/src/it/polito/tdp/alien/AlienController.java
+++ Lab02_Alien/src/it/polito/tdp/alien/AlienController.java
@@ -8,6 +8,7 @@ package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,6 +17,8 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class AlienController {
+	
+	private alienDictionary alienDictionary = new alienDictionary();
 	
     @FXML
     private ResourceBundle resources;
@@ -43,12 +46,41 @@ public class AlienController {
     
     @FXML
     void doTranslate(ActionEvent event) {
+    	String input = txtWord.getText().toLowerCase();
+    	StringTokenizer st = new StringTokenizer(input, " ");
+    	String alien = st.nextToken();
+    	if (st.hasMoreTokens()) {
+    		String traduzione = st.nextToken();
+    		if (!alien.matches("[a-zA-Z]*") || !traduzione.matches("[a-zA-Z]*")) {
+				txtResult.setText("Inserire solo caratteri alfabetici.");
+				return;
+			}
+    		alienDictionary.addWord(alien, traduzione);
+    		txtResult.setText("La parola: \"" + alien + "\", con traduzione: \"" + traduzione + "\", è stata inserita nel dizionario.");
+    		txtWord.clear();
+    	}
+    	else{
+    		if (!alien.matches("[a-zA-Z]*")) {
+				txtResult.setText("Inserire solo caratteri alfabetici.");
+				return;
+			}
+    		String translation = alienDictionary.translateWord(alien);
+    		if (translation != null) {
+    			translation = translation.replaceAll(" \n", System.getProperty("line.separator"));
+				txtResult.setText(translation);
+			} else {
+				txtResult.setText("La parola cercata non esiste nel dizionario.");
+			}
+    		txtWord.clear();
+    	}
     	    	
     }
     
     
     @FXML
     void doReset(ActionEvent event) {
+    	txtResult.clear();
+    	alienDictionary.resetDictionary();
 
     }
     
